7fdc6bf077146c790eddfd91c22df4c8ed5d9dc9
marcus <marcus@hhost.me>
2020-01-10 00:25:32 +0000

    testing

--- index.vue
+++ index.vue
@@ -1,19 +1,38 @@
 <template>
-   <div class="keyboard">
-     <NumericKeyboard layout="tel" @press="press" />
-   </div>
+  <div class="keyboard">
+    <div class="grid">
+      <div v-for="(v,k) in cat ":key="k" >{{v}}</div>
+      <div class="gridb">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+        <div>0</div>
+        <div>.</div>
+        <div><</div>
+      </div>
+    </div>   
+  </div>
 </template>
 
 <script>
-  import { NumericKeyboard } from 'numeric-keyboard'
   export default {
     name: "v-pos",
     components: {
-      NumericKeyboard
     },
     methods: {
       press(key) {
       }
-    }
+    },
+    data(){
+      return {
+	cat:["a","b","c"]
+      }
+    },
   }
 </script>
